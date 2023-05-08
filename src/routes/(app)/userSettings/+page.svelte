d93e327a4073bb9c6ad0ccb8ec6feb774ba1d1f4
Niels Thuis <cnops_niels@hotmail.com>
2023-05-09 00:07:48 +0200

    redirecting to login page if user is not set

--- src/routes/(app)/userSettings/+page.svelte
+++ src/routes/(app)/userSettings/+page.svelte
@@ -1,20 +1,30 @@
 <script>
-  import { user } from "../../../components/user-store.js";
+  import { goto } from "$app/navigation";
+  import { userStore } from "../../../components/user-store";
+  const user = userStore.getInfo();
+  console.log(user);
+  if (!user.id) {
+    goto("/login");
+  }
 </script>
 
 <main>
   <h1>User Information</h1>
   <ul>
-    <li><strong>Id:</strong> {$user.id}</li>
-    <li><strong>Name:</strong> {$user.name}</li>
-    <li><strong>Role:</strong> {$user.role}</li>
+    <li><strong>Id:</strong> {user.id}</li>
+    <li><strong>Name:</strong> {user.name}</li>
+    <li><strong>Role:</strong> {user.role}</li>
     <li>
-      <strong>Permissions:</strong>
-      <ul>
-        {#each $user.permissions as permission}
-          <li>{permission}</li>
-        {/each}
-      </ul>
+      {#if user.permissions && user.permissions.length > 0}
+        <ul>
+          <strong>Permissions:</strong>
+          {#each user.permissions as permission}
+            <li>{permission}</li>
+          {/each}
+        </ul>
+      {:else}
+        <p>No permissions found.</p>
+      {/if}
     </li>
   </ul>
 </main>
